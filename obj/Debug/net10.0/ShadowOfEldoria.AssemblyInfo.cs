7aa1a4431d5c5748a97b04453f5b5cf140980293
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-12-12 13:18:19 +0100

    Update assembly information and add sourcelink support for better debugging

--- obj/Debug/net10.0/ShadowOfEldoria.AssemblyInfo.cs
+++ obj/Debug/net10.0/ShadowOfEldoria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShadowOfEldoria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8cbe94b18c434476833cae70090fd3961858e9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShadowOfEldoria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShadowOfEldoria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
